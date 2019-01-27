ff0ae40277c1a7eb851afce1bf2e1ca6f8a8b190
lzx <123456789>
2019-01-27 18:11:39 +0800

    1

--- hanoi.py
+++ hanoi.py
@@ -1,15 +1,29 @@
 # coding:utf8
 
 
-def hanoi(num):
-    if num >= 1:
-        if num == 1:
+def hanoi(n):
+    if n >= 1:
+        if n == 1:
             return 1
         else:
-            return hanoi(num - 1) * 2 + 1
+            return hanoi(n - 1) * 2 + 1
     else:
         return 'please input a number >= 1'
 
 
+def hanoi_steps(n, x, y, z):
+    if n >= 0:
+        if n == 0:
+            pass
+        else:
+            hanoi_steps(n - 1, x, z, y)
+            print('%s -> %s' % (x, y))
+            hanoi_steps(n - 1, z, y, z)
+    else:
+        return 'please input a number >= 0'
+
+
 if __name__ == '__main__':
-    print(hanoi(0))
+    n = 3
+    print(hanoi(n))
+    hanoi_steps(n, 'a', 'b', 'c')
